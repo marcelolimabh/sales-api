6688a97eda9f4f871a4797a1b17484a294a1f61e
Marcelo Bicalho <marcelolimabh@gmail.com>
2022-09-17 14:42:11 -0300

    Add existing project files to Git

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import { Category } from "../model/Category";
+
+const categories = [];
+const categoriesRoutes = Router();
+
+categoriesRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+  const category: Category = new Category();
+  Object.assign(category, { name, description, created_at: new Date() });
+  categories.push(category);
+  return res.status(201).send();
+});
+export { categoriesRoutes };
