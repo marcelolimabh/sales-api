8628067f509445d0b9c613bbdff13341ba085038
Marcelo Bicalho <marcelolimabh@gmail.com>
2022-09-17 15:31:41 -0300

    listar categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,13 +2,21 @@ import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
-const categories = [];
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
-  const categoriesRepository = new CategoriesRepository();
+
   categoriesRepository.create({ name, description });
+
   return res.status(201).send();
 });
+
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+
+  return res.json(all);
+});
+
 export { categoriesRoutes };
