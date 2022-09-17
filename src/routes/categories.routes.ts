df2bbe0bf303043de817ad620c1c5fabf0c6f0b7
Marcelo Bicalho <marcelolimabh@gmail.com>
2022-09-17 15:24:48 -0300

    criação do repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categories = [];
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
-  const category: Category = new Category();
-  Object.assign(category, { name, description, created_at: new Date() });
-  categories.push(category);
-  return res.status(201).json({ category });
+  const categoriesRepository = new CategoriesRepository();
+  categoriesRepository.create({ name, description });
+  return res.status(201).send();
 });
 export { categoriesRoutes };
