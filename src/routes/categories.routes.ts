28efba17f1aeae34e953a90bd59843dc3be39c8b
Marcelo Bicalho <marcelolimabh@gmail.com>
2022-09-18 16:51:08 -0300

    usecase listCategories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { ICategoriesRepository } from "../modules/cars/repositories/ICategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
+import { listCategoryController } from "../modules/cars/useCases/listCategories/index";
 
 const categoriesRoutes = Router();
-const categoriesRepository: ICategoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
-  const all = categoriesRepository.list();
-
-  return res.json(all);
+  return listCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
