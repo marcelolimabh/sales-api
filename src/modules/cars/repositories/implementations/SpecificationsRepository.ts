a3ba7ae3bc2d35f2076a61575c1ecd01f2a27bbe
Marcelo Bicalho <marcelolimabh@gmail.com>
2022-09-18 17:37:15 -0300

    implementando singleton specification

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,17 +1,28 @@
-import { Specification } from "../model/Specification";
+/* eslint-disable no-use-before-define */
+import { Specification } from "../../model/Specification";
 import {
   ICreateSpecificationDTO,
   ISpecificationsRepository,
-} from "./ISpecificationsRepository";
+} from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
   specifications: Specification[];
-  constructor() {
+
+  private static INSTANCE: SpecificationsRepository;
+  private constructor() {
     this.specifications = [];
+  }
+
+  public static getInstacnce(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+    return SpecificationsRepository.INSTANCE;
   }
   findByName(name: string): Specification {
     return this.specifications.find((s) => s.name === name);
   }
+
   create({ name, description }: ICreateSpecificationDTO): void {
     const specification = new Specification();
     Object.assign(specification, { name, description, created_at: new Date() });
