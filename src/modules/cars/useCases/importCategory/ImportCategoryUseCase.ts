0fbfeadae4eb94451e71945542361ce1ed43e1fc
Marcelo Bicalho <marcelolimabh@gmail.com>
2022-09-18 20:00:30 -0300

    use stream read file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,15 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  execute(file: any): void {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParse({});
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line: string) => {
+      console.log(line);
+    });
   }
 }
 export { ImportCategoryUseCase };
